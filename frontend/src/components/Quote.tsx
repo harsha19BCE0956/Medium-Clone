d0787c269bcceaa64069c55514c12462404dc2f4
harsha19BCE0956 <dharsha1234@gmail.com>
2024-12-30 02:58:02 -0800

    frontend done

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,15 @@
+export const Quote = () => {
+    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-3xl font-bold">
+                    “Blogging is to writing what extreme sports are to athletics: more free-form, less formal, more alive.”
+                </div>
+                <div className="max-w-md text-xl font-semibold text-left mt-4">
+                    Andrew Sullivan
+                </div>
+            </div>
+        </div>
+        
+    </div>
+}
